a30960b648e83f41f87e69eaa5f5d6f565244e57
KingToochi <tu@byupathway.edu>
2025-04-30 15:37:38 +0100

    update

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -17,7 +17,7 @@ const Login = () => {
                     <i className="fa fa-lock"></i>
                     <input className='w-[97.5%] border-0 focus:outline-none' type="password" name="password" placeholder="password"/>
                 </label>
-                <button className='border rounded-[10px] w-[20%] m-auto bg-white cursor-pointer' type='submit'>Login</button>
+                <button className='border-2 rounded-[10px] w-[20%] m-auto bg-white cursor-pointer' type='submit'>Login</button>
                 <label className='flex ml-[50px]'>
                     <input type="checkbox" checked="checked" name="remember" /> Remember me
                     <span class="ml-[250px]">Forgot <Link href="#">password?</Link></span>
